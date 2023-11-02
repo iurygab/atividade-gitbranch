854704a95b388d78e7b1c6ca35551437c0421399
iurygab <iury.gabriel.r@gmail.com>
2023-11-01 21:32:50 -0400

    mensagem

--- gitbranch/src/App.java
+++ gitbranch/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        for(int i=0;i<4;i++){
+            System.out.println("Hello, World!");
+        }
+        
+       
     }
 }
